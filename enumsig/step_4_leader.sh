9128b174381319afd7d06bc24f11a0445aeb9b28
ubuntu <ubuntu@mail.eosname.cn>
2018-11-05 20:25:44 +0800

    update

--- enumsig/step_4_leader.sh
+++ enumsig/step_4_leader.sh
@@ -4,4 +4,4 @@ RPC="https://filter.qsx.io"
 # after the leader collect enough signatures of the BP's, than he/she
 # fill the signatures in the upgrade_enumsig_contract_official_trx.json and 
 # rename to upgrade_enumsig_contract_official_signed_trx.json, then push to the blockchain
-enucli -u $RPC push transaction --skip-sign upgrade_enumsig_contract_official_signed_trx.json
+enucli -u $RPC push transaction --skip-sign upgrade_enumsig_contract_signed_trx.json
